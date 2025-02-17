cbaa1b050eba1d9867e4df5644ecad6c8aa0702b
Ismael Moreira <ismaelmoreirakt@gmail.com>
2025-02-17 17:18:58 +0000

    feat: Addition of the user input reading function.

--- commiter/src/main.rs
+++ commiter/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+fn read_input(prompt: &str) -> String {
+    let mut input = String::new();
+    print!("{}", prompt);
+    io::stdin().read_line(&mut input).unwrap().expect("Failed to read input");
+    input.trim().to_string()
 }
